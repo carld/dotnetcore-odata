f5e9a31ccaa8681683236d8e7ffd6cecd59ddeea
Carl <105003+carld@users.noreply.github.com>
2021-07-21 23:10:10 +1200

    Add a WeatherForecast model

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace odata_service.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int Temperature { get; set; }
+
+        public string summary { get; set; }
+    }
+}
